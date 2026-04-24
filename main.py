22089f4b5d00b995541ee9b0b6b61260dcc201e1
Jinxxxok <hyeokjiny01@gmail.com>
2026-04-24 18:53:19 +0900

    Root dir update

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-
-Car_info = pd.read_csv("./csv/encar_cars_v2.csv")
-
-print(Car_info.shape())
-
-print(Car_info.head())
+# main.py
